6299ef9c68a908f788cae2f00ce11e810e7584a8
jollyjonson <jonas.hajek-lellmann@gmx.de>
2023-12-12 17:34:01 +0100

    Reorder member declarations for clarity

--- frechet_audio_distance/fad_statistics.py
+++ frechet_audio_distance/fad_statistics.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 
 class Statistics:
     def __init__(self, dim: int):
-        self.covariance = tf.Variable(tf.zeros((dim, dim), dtype=tf.float64))
         self.mean = tf.Variable(tf.zeros((dim,), dtype=tf.float64))
+        self.covariance = tf.Variable(tf.zeros((dim, dim), dtype=tf.float64))
+
         self.num_items_processed = tf.Variable(0.0, dtype=tf.float64)
 
     def update(self, data: tf.Tensor) -> None:  # pragma: no cover
